668cc0dcb5233c106fc146cba8f62ab73f745ebd
maatko <matkovukovic25@gmail.com>
2025-02-02 05:27:12 +0100

    changed neotree config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,13 +1,19 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    "3rd/image.nvim",
-  },
-  config = function()
-    vim.keymap.set("n", "<C-h>", ":Neotree filesystem toggle left<CR>", {})
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			source_selector = {
+				winbar = true,
+				statusline = false,
+			},
+		})
+		vim.keymap.set("n", "<C-h>", ":Neotree filesystem toggle left<CR>", {})
+	end,
 }
