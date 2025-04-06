08bd2c83d78a8945a15f0db81ba3dff54036e721
maatko <matkovukovic25@gmail.com>
2025-04-06 03:04:53 +0200

    finally configured dap-ui

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -5,7 +5,22 @@ return {
     "rcarriga/nvim-dap-ui",
   },
   config = function()
-    local dap = require("dap")
+    local dap, dapui = require("dap"), require("dapui")
+    dapui.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
     vim.keymap.set("n", "<F5>", function()
       dap.continue()
     end)
