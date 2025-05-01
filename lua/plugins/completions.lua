eb3356bc56a73017cba02541c4b620b5e2d10233
maatko <matkovukovic25@gmail.com>
2025-05-01 21:38:53 +0200

    fixed blink.cmp keybinds

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,23 +1,22 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets" },
-  version = "1.*",
-  opts = {
-    keymap = {
-      preset = "default",
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "cancel", "hide" },
-      ["<CR>"] = { "select_and_accept" },
-      ["<Up>"] = { "select_prev" },
-      ["<Down>"] = { "select_next" },
-      ["<Tab>"] = { "snippet_forward" },
-      ["<S-Tab>"] = { "snippet_backward" },
-    },
-    appearance = { nerd_font_variant = "mono" },
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
-  },
-  opts_extend = { "sources.default" },
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "1.*",
+	opts = {
+		keymap = {
+			preset = "none",
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+			["<Esc>"] = { "cancel", "fallback" },
+			["<CR>"] = { "select_and_accept", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+		},
+		appearance = { nerd_font_variant = "mono" },
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+	},
+	opts_extend = { "sources.default" },
 }
