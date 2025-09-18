54722c39659bcc55ccce0a0808d6bdbebfdd19fe
maatko <matkovukovic25@gmail.com>
2025-09-18 17:21:47 +0200

    fixed formatting for lua & python, also updated default font weight of alacritty

--- nvim/lua/plugins/nonels.lua
+++ nvim/lua/plugins/nonels.lua
@@ -12,5 +12,13 @@ return {
         "black"
       }
     })
+
+    local null_ls = require("null-ls");
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
+      },
+    })
   end,
 }
