8a4214723bad11c620d03f63f15e691cc693becf
maatko <matkovukovic25@gmail.com>
2025-09-20 03:15:19 +0200

    added typescript language server in ensured installed

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,42 +1,43 @@
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "tailwindcss",
-        "omnisharp",
-        "emmet_ls",
-        "pyright",
-        "lua_ls",
-        "clangd",
-        "cssls",
-        "vimls",
-        "gopls",
-        "templ",
-        "html",
-      }
-    },
-    dependencies = {
-      {
-        "neovim/nvim-lspconfig",
-        config = function()
-          local telescope = require("telescope.builtin")
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"tailwindcss",
+				"omnisharp",
+				"emmet_ls",
+				"pyright",
+				"lua_ls",
+				"clangd",
+				"ts_ls",
+				"cssls",
+				"vimls",
+				"gopls",
+				"templ",
+				"html",
+			},
+		},
+		dependencies = {
+			{
+				"neovim/nvim-lspconfig",
+				config = function()
+					local telescope = require("telescope.builtin")
 
-          -- lsp
-          vim.keymap.set({ "n", "v" }, "<leader>g", vim.lsp.buf.code_action, {})
-          vim.keymap.set("n", "<leader>d", vim.lsp.buf.hover, {})
-          vim.keymap.set("n", "<C-g>", vim.lsp.buf.definition, {})
-          vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+					-- lsp
+					vim.keymap.set({ "n", "v" }, "<leader>g", vim.lsp.buf.code_action, {})
+					vim.keymap.set("n", "<leader>d", vim.lsp.buf.hover, {})
+					vim.keymap.set("n", "<C-g>", vim.lsp.buf.definition, {})
+					vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
 
-          -- telescope
-          vim.keymap.set("n", "<leader>r", telescope.lsp_references, {})
-          vim.keymap.set("n", "<leader>i", telescope.lsp_implementations, {})
-        end
-      },
-      {
-        "mason-org/mason.nvim",
-        opts = {}
-      },
-    },
-  }
+					-- telescope
+					vim.keymap.set("n", "<leader>r", telescope.lsp_references, {})
+					vim.keymap.set("n", "<leader>i", telescope.lsp_implementations, {})
+				end,
+			},
+			{
+				"mason-org/mason.nvim",
+				opts = {},
+			},
+		},
+	},
 }
