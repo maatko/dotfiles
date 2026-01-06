46cd9ca8cc2c451a8090dbddc62fb23b7b9c4a91
maatko <matkovukovic25@gmail.com>
2026-01-06 22:21:06 +0100

    added custom c snipper for header guards & version bump

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,16 +1,17 @@
 return {
-  'saghen/blink.cmp',
-  version = '1.*',
-  opts = {
-    keymap = { preset = 'default' },
-    completion = { documentation = { auto_show = false } },
-    sources = {
-      default = {
-        'lsp',
-        'path',
-        'snippets',
-        'buffer'
-      },
-    },
-  }
+	"saghen/blink.cmp",
+	version = "1.*",
+	opts = {
+		keymap = { preset = "default" },
+		completion = { documentation = { auto_show = false } },
+		snippets = { paths = { vim.fn.stdpath("config") .. "/snippets" } },
+		sources = {
+			default = {
+				"lsp",
+				"path",
+				"snippets",
+				"buffer",
+			},
+		},
+	},
 }
